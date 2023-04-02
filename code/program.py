b54c6acf62c8a78fa28e6e0581997731df02aadf
a <a@b.c>
2023-04-02 23:27:28 +0300

    modified

--- code/program.py
+++ code/program.py
@@ -1,7 +1,6 @@
 def digit_sum(num):
     return sum(int(i) for i in str(num))
 
-
 s = 0
 for i in range(1000, 10000):
     if digit_sum(i) == 20:
@@ -9,3 +8,4 @@ for i in range(1000, 10000):
 print(s)   
 
 
+print(tuple(range((n := int(input())) + (n + 1)%2, n*n + 1, 2)))
